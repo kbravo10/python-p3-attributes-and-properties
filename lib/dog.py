52ed5a17286ff648dde7c6ee359821f1abb3e57b
Kevin Bravo <kbravo1024@gmail.com>
2023-07-08 15:00:07 -0700

    complete attribute and property lab

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,33 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name= None, breed = None):
+        if(name != None):
+            if(type(name) == str):
+                if( 1 < len(name) < 25):
+                    print(f'Setting name to {name}.')
+                    self._name = name
+                else:
+                    print("Name must be string between 1 and 25 characters.")
+            else:
+                print("Name must be string between 1 and 25 characters.")
+        if(breed != None):
+            loacted = False
+            for breeds in APPROVED_BREEDS:
+                if(breed == breeds):
+                    self._breed = breed
+                    loacted = True
+            if (loacted == False):
+                print("Breed must be in list of approved breeds.")
+                
+        
+
+    def get_name(self):
+        print("Retrieving name.")
+        return self._name
+    
+    def get_breed(self):
+        return self._breed
+    
+    name = property(get_name,__init__)
+    breed = property(get_breed,__init__)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,27 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = None, job = None):
+        if(name != None):
+            if(type(name) == str and 1 < len(name) < 25):
+                name.upper()
+                self._name = name.title()
+            else:
+                print("Name must be string between 1 and 25 characters.")
+        if(job != None):
+            located = False
+            for jobs in APPROVED_JOBS:
+                if(jobs == job):
+                    located = True
+                    self._job = job
+            if(located != True):
+                print("Job must be in list of approved jobs.")
+
+    def get_name(self):
+        return self._name
+
+    def get_job(self):
+        return self._job
+
+    name = property(get_name, __init__)
+    job = property(get_job, __init__)
